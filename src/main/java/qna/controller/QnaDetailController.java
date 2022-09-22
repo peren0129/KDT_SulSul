9d4f0f8cd413de4e275bcd3aa10acd2d497f7902
HyangJin Lee <perenata@naver.com>
2022-09-22 11:39:39 +0900

    220922_1140

--- src/main/java/qna/controller/QnaDetailController.java
+++ src/main/java/qna/controller/QnaDetailController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import qna.model.BoardService;
 import qna.model.QnaBean;
 import qna.model.QnaDao;
 
@@ -40,52 +39,6 @@ public class QnaDetailController {
 		mav.setViewName(getPage);
 		return mav;
 	}
-	/*
-    @RequestMapping(value = "/board/view")
-    public String boardView(@RequestParam Map<String, Object> paramMap, Model model) {
- 
-        model.addAttribute("replyList", boardService.getReplyList(paramMap));
-        model.addAttribute("boardView", boardService.getContentView(paramMap));
- 
-        return "boardView";
- 
-    }
-    */
 }
-
-
-
-
-
-
-
-
-/*
-@Controller
-public class QnaDetailController {
-	private final String command = "detail.qna";
-	private String getPage = "/detailForm";
-	
-	@Autowired
-	QnaDao qnaDao;
-	
-	@RequestMapping(command)
-	public String content(Model model, @RequestParam("num") String num,
-			@RequestParam("pageNumber") String pageNumber) {
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-		
-		qnaDao.updateReadcount(num);
-		QnaBean qna = qnaDao.getData(num);
-		
-		model.addAttribute("pageNumber", pageNumber);
-		model.addAttribute("qna", qna);
-		
-		return getPage;
-	}
-}
- */
-
-
 
 
